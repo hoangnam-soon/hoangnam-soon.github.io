a5189a9f81964f39abc319f69e2f15593d3ef3df
hoangnam-soon <hoangnam1739@gmail.com>
2021-03-23 18:01:21 +0700

    ok
    ok

---
+++ list student/index.js
@@ -0,0 +1,229 @@
+$(function () {
+
+
+
+
+// Hàm search
+  $('#search-name').keyup(function (event) {
+    if (event.which === 13) {
+      // xóa body cũ thay body mới là rỗng
+      $("#body-item").html("");
+      $.get(`http://localhost:3000/dataList?name_like=${event.target.value}`, function (data, status) {
+        data.forEach(item => {
+          $("#body-item").append(`
+                        <tr>
+                          <td>${item.name}</td>
+                          <td>${item.age}</td>
+                          <td>${item.email}</td>
+                          <td>${item.description}</td>
+                          <td class="delete-data"  data-id=${item.id}><i class="fas fa-trash"></i></td>
+                          <td class="update-data"  data-id=${item.id} ><i class="fas fa-user-edit"></i></td>
+                        </tr>
+                        `)
+        });
+      })
+
+
+
+
+    }
+
+
+  })
+
+
+
+  const reUpdateDom = () => {
+    $.get(`http://localhost:3000/dataList`, function (data, status) {
+
+      let newDom = ''
+      data.forEach(item => {
+        newDom = newDom + `
+        <tr>
+          
+          <td>${item.name}</td>
+          <td>${item.age}</td>
+          <td>${item.email}</td>
+          <td>${item.description}</td>
+          <td class="delete-data"  data-id=${item.id} ><i class="fas fa-trash"></i></td>
+          <td class="update-data"  data-id=${item.id}><i class="fas fa-user-edit"></i></td>
+          
+        </tr>
+        `
+      })
+      let newBody = ` <tbody id="body-item">${newDom}</tbody>`
+      $('#body-item').replaceWith(newBody)
+      listenClick()
+
+
+
+
+    }
+
+
+    )
+  }
+
+  const listenClick = () => {
+    $(".delete-data").click(function (event) {
+      let { id } = event.currentTarget.dataset
+      $.ajax({
+        url: `http://localhost:3000/dataList/${id}`,
+        type: 'DELETE',
+        success: function (result) {
+
+          // replaceWith lại body
+          reUpdateDom()
+
+        }
+      });
+    })
+    $(".update-data").click(function (event) {
+      let { id, name,age,email } = event.currentTarget.dataset
+      $('#exampleModal').modal('show');
+      let field_name = $('#field-name').val(name)
+      let field_age = $('#field-age').val(age)
+      let field_email=$('#field-email').val(email)
+      
+
+     
+
+    })
+  }
+
+  $('#onUpdate').click(function (event) {
+    let { id } = event.currentTarget.dataset
+    
+    let field_name = $('#field-name').val()
+    let field_age = $('#field-age').val()
+    let field_email=$('#field-email').val()
+
+    $.ajax({
+      type:  'PUT',
+      url: `http://localhost:3000/dataList/${id}`,
+      data: {
+        field_name,
+        field_age,
+        field_email,
+        description: "i'm " + field_name
+      },
+      success: function (result) {
+        console.log('result',result)
+        // debugger
+        // $('#exampleModal').modal('toggle')
+        // reUpdateDom()
+      }
+    });
+
+  })
+
+
+
+
+
+
+
+
+  $.get(`http://localhost:3000/dataList`, function (data, status) {
+    console.log("data", data);
+
+    data.forEach(item => {
+      $("#body-item").append(`
+                    <tr>
+                      <td>${item.name}</td>
+                      <td>${item.age}</td>
+                      <td>${item.email}</td>
+                      <td>${item.description}</td>
+                      <td class="delete-data"  data-id=${item.id}><i class="fas fa-trash"></i></td>
+                      <td class="update-data"  data-id=${item.id} data-name=${item.name} data-age=${item.age} data-email=${item.email} data-description=${item.description}><i class="fas fa-user-edit"></i></td>
+                    </tr>
+                    `)
+    });
+    listenClick()
+
+  });
+
+
+
+  $('#onAdd').click(function (event) {
+    let { id } = event.currentTarget.dataset 
+    // lay data cua 3 field
+    let name = $('#field-name').val()
+    let age = $('#field-age').val()
+    let email = $('#field-email').val()
+
+    $.ajax({
+      type:  "POST",
+      url: "http://localhost:3000/dataList",
+      data: {
+        name,
+        age,
+        email,
+        description: "i'm " + name
+      },
+      success: function (result) {
+        $('#exampleModal').modal('toggle')
+        reUpdateDom()
+      }
+    });
+
+  })
+
+
+
+
+  // typescript ---> Ngôn ngữ hướng đối tượng
+
+
+
+
+
+})
+
+
+
+
+
+
+
+// // var [{prop:x}]=[]
+// var [{prop:x} = {prop:123}]=[]
+// console.log('x',x)
+
+
+// // khác nhau giữa let và var
+// if(true){
+//     var d =2 
+// }
+// console.log('d',d)
+// if(false){
+//     let c = 3
+// }
+// console.log('c',c)
+
+// // var sẽ đưa lên đầu còn let thì không
+// console.log('a',a)
+ 
+// var a = 'ok'
+
+
+// const na={
+//     name: 'nam'
+//     age : 17
+// }
+// na.age=16
+// console.log('na',na)
+// // hàm map là trả về một mảng mới tương ứng độ dài với mảng cũ mỗi lần lặp sẽ trả về một gì đó
+// let arr=[1,2,3,4]
+
+// // let a = arr.map((item,index) =>{
+// //     return{
+// //         key : index,
+// //         value : item
+// //     }
+// // })
+// // console.log('a',a)
+
+// // // tương tự ở es6
+// let retunObj= (arr) => arr.map((item,index) => ({key:index,value:item}))
+// console.log('retunObj',retunObj(arr))
---
+++ list student/shukudai.js
@@ -0,0 +1,127 @@
+// Bài 1
+
+// Viết hàm getDateAgo(date, days) để hiển thị ngày trong tuần ở định dạng ngắn gọn: ‘MO’, ‘TU’, ‘WE’, ‘TH’, ‘FR’, ‘SA’, ‘SU’.
+
+const getdays = () => {
+    var date = new Date()
+    var weekday = new Array(7);
+    weekday[0] = "Sunday";
+    weekday[1] = "Monday";
+    weekday[2] = "Tuesday";
+    weekday[3] = "Wednesday";
+    weekday[4] = "Thursday";
+    weekday[5] = "Friday";
+    weekday[6] = "Saturday";
+
+
+     var today = weekday[date.getDay()]
+     return today
+}
+alert('Today is ' + getdays())
+console.log('today',getdays())
+
+
+
+
+
+
+
+// Bài 2
+
+// Tạo một hàm getDateAgo(date, days) để trả về ngày của tháng, ngày trước kể từ ngày.
+
+// Ví dụ: nếu hôm nay là ngày 20, thì getDateAgo(new Date(), 1) phải là 19 và getDateAgo(new Date(), 2) phải là 18.
+
+// Nên hoạt động đúng trong days = 365 hoặc hơn:
+
+// let date = new Date(2015, 0, 2);
+
+// alert( getDateAgo(date, 1) ); // 1, (1 Jan 2015)
+// alert( getDateAgo(date, 2) ); // 31, (31 Dec 2014)
+// alert( getDateAgo(date, 365) ); // 2, (2 Jan 2014)
+
+const daysAgo = () =>{
+    var date = new Date()
+    var daysAgo = new Date(date)
+    daysAgo.setDate(date.getDate()-1)
+   
+    return daysAgo
+}
+alert('Yesterday is ' + daysAgo())
+
+
+
+
+
+// Bài 3
+
+// Viết một hàm getLastDayOfMonth(year, month) trả về ngày cuối cùng của tháng. Đôi khi đó là ngày 30, 31 hoặc thậm chí 28/29 đối với tháng 2.
+
+// Thông số:
+
+// year – năm có bốn chữ số, ví dụ như năm 2012.
+// year – tháng, từ 0 đến 11.
+// Ví dụ: getLastDayOfMonth(2012, 1) = 29 (năm nhuận, tháng 2).
+
+const getLastDayOfMonth = (year,month) =>{
+    return new Date(year,month,0).getDate()
+
+}
+alert('Last day of February in 2020 is :' + getLastDayOfMonth(2020,2))
+
+
+
+
+// Bài 4
+
+// Tạo một hàm getSecondsToTomorrow() trả về số giây cho đến ngày mai.
+
+
+
+const getSecondsToTomorrow =() =>{
+    var date = new Date()
+    var tomorrow = new Date(date)
+   tomorrow.setDate(date.getDate()+1)
+    var  timeToday = date.getTime()
+    var timetomorrow = tomorrow.getTime()
+    var timeCountdown = timetomorrow - timeToday
+    // var days = Math.floor(timeCountdown / (1000 * 60 * 60 * 24));
+    // var hours = Math.floor((timeCountdown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    // var minutes = Math.floor((timeCountdown % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((timeCountdown /1000));
+
+   
+    return seconds
+}
+
+alert('Còn ' + getSecondsToTomorrow() + 's là đến ngày mai')
+// Bài 5
+
+// Viết một hàm formatDate(date) sẽ định dạng date như sau:
+
+// Nếu kể từ ngày trôi qua dưới 1 giây, thì “ngay bây giờ”.
+// Ngược lại, nếu kể từ ngày trôi qua dưới 1 phút, thì “n giây trước”.
+// Ngược lại, nếu ít hơn một giờ, thì “m min. Ago”.
+// Nếu không, ngày đầy đủ ở định dạng “DD.MM.YY HH: mm”. Đó là: “day.month.year hours: minutes”, tất cả đều ở định dạng 2 chữ số, ví dụ: 31.12.16 10:00.
+// Ví dụ:
+
+// alert( formatDate(new Date(new Date - 1)) ); // "right now"
+
+// alert( formatDate(new Date(new Date - 30 * 1000)) ); // "30 sec. ago"
+
+// alert( formatDate(new Date(new Date - 5 * 60 * 1000)) ); // "5 min. ago"
+
+// // yesterday's date like 31.12.16 20:00
+// alert( formatDate(new Date(new Date - 86400 * 1000)) );
+// Bài 6
+
+// Viết hàm tính toán chênh lệch giữa 2 ngày bất kỳ ở dạng ( dd/mm/yyy hoặc mm/dd/yyyy). Check xem chênh lệch bao nhiêu ngày 
+var date1 = new Date("10/11/1995"); 
+var date2 = new Date("10/11/2021"); 
+  
+
+var Difference_In_Time = date2.getTime() - date1.getTime(); 
+  
+
+var Difference_In_Days = Difference_In_Time / (1000 * 3600 * 24); 
+alert (Difference_In_Days)
